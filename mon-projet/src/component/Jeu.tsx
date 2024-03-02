17c6a7f108ec65fda6758f7e5f64974e7618bb31
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2024-03-02 17:42:23 +0100

    Update Jeu.tsx

--- mon-projet/src/component/Jeu.tsx
+++ mon-projet/src/component/Jeu.tsx
@@ -5,6 +5,8 @@ export const Jeu = () => {
   const [Cards, setCards] = useState<string[]>(["2-C.png", "2-D.png", "2-H.png", "2-S.png", "3-C.png", "3-D.png", "3-H.png", "3-S.png", "4-C.png", "4-D.png", "4-H.png", "4-S.png", "5-C.png", "5-D.png", "5-H.png", "5-S.png", "6-C.png", "6-D.png", "6-H.png", "6-S.png", "7-C.png", "7-D.png", "7-H.png", "7-S.png", "8-C.png", "8-D.png", "8-H.png", "8-S.png", "9-C.png", "9-D.png", "9-H.png", "9-S.png", "10-C.png", "10-D.png", "10-H.png", "10-S.png", "A-C.png", "A-D.png", "A-H.png", "A-S.png", "J-C.png", "J-D.png", "J-H.png", "J-S.png", "Q-C.png", "Q-D.png", "Q-H.png", "Q-S.png", "K-C.png", "K-D.png", "K-H.png", "K-S.png"]);
   const [dealerCards, setDealerCards] = useState<string[]>([]);
   const [playerCards, setPlayerCards] = useState<string[]>([]);
+  const [dealerScore, setDealerScore] = useState<number>(0);
+  const [playerScore, setPlayerScore] = useState<number>(0);
 
   const hit = (j: string[]) => {
     let randomIndex = Math.floor(Math.random() * Cards.length);
@@ -24,38 +26,60 @@ export const Jeu = () => {
     setCards(updatedCards);
   };
 
-  const deal = (j: string[]) => {
+  const updatePlayerScore = (cards: string[]) => {
+    let tempPlayerScore : number = playerScore;
+   for (let i = 0; i < cards.length; i++) {
+    let cardValue = cards[i].slice(0, cards[i].indexOf("-"));
+    if (cardValue === "A") {
+      tempPlayerScore += 11;
+    } else if (cardValue === "J" || cardValue === "Q" || cardValue === "K") {
+      tempPlayerScore += 10;
+    } else {
+      tempPlayerScore += parseInt(cardValue);
+    }
+  };
+  setPlayerScore(tempPlayerScore);
+  };
+
+  const updateDealerScore = (cards: string[]) => {
+    let tempdealerScore : number  = dealerScore;
+    for (let i = 0; i < cards.length; i++) {
+    let cardValue = cards[i].slice(0, cards[i].indexOf("-"));
+    if (cardValue === "A") {
+      tempdealerScore += 11;
+    } else if (cardValue === "J" || cardValue === "Q" || cardValue === "K") {
+      tempdealerScore += 10;
+    } else {
+      tempdealerScore += parseInt(cardValue);
+    }
+  };
+  setDealerScore(tempdealerScore);
+  };
+
+  const deal = () => {
+    dealerCards : [] = [];
+    playerCards : [] = [];
+    let updatedCards = [...Cards];
+    for(let i = 0; i < 2; i++) {
     let randomIndex = Math.floor(Math.random() * Cards.length);
     let cardToAdd: string = Cards[randomIndex];
-    let updatedCards = [...Cards];
-
     updatedCards.splice(randomIndex, 1);
-
-    if (j === dealerCards) {
-      setDealerCards([...dealerCards, cardToAdd]);
-      console.log(cardToAdd)
-      console.log(dealerCards)
-    } else if (j === playerCards) {
-      setPlayerCards([...playerCards, cardToAdd]);
+    dealerCards.push(cardToAdd);
     }
+    updateDealerScore(dealerCards);
+    setDealerCards(dealerCards);
 
+    for(let i = 0; i < 2; i++) {
+    let randomIndex = Math.floor(Math.random() * Cards.length);
+    let cardToAdd: string = Cards[randomIndex];
+  
+    updatedCards.splice(randomIndex, 1);
+    playerCards.push(cardToAdd);
+    }
+    updatePlayerScore(playerCards);
+    setPlayerCards(playerCards);
     setCards(updatedCards);
 
-    let randomIndex2 = Math.floor(Math.random() * Cards.length);
-    let cardToAdd2: string = Cards[randomIndex];
-    let updatedCards2 = [...Cards];
-
-    updatedCards.splice(randomIndex2, 1);
-
-    if (j === dealerCards) {
-      setDealerCards([...dealerCards, cardToAdd2]);
-      console.log(cardToAdd2)
-      console.log(dealerCards)
-    } else if (j === playerCards) {
-      setPlayerCards([...playerCards, cardToAdd2]);
-    }
-
-    setCards(updatedCards2);
   };
 
   const stand = () => {
@@ -67,7 +91,7 @@ export const Jeu = () => {
       <h1>Blackjack Game</h1>
       {/* Afficher les cartes du joueur */}
       <div>
-        <h2>Player's Hand</h2>
+        <h2>Player's Hand {playerScore}</h2>
         {playerCards.map((card, index) => (
           <div key={index}>
             <img src={"/cards/" + card} alt={`Card ${index}`} />
@@ -76,7 +100,7 @@ export const Jeu = () => {
       </div>
       {/* Afficher les cartes du croupier */}
       <div>
-        <h2>Dealer's Hand</h2>
+        <h2>Dealer's Hand {dealerScore}</h2>
         {dealerCards.map((card, index) => (
           <div key={index}>
             <img src={"/cards/" + card} alt={`Card ${index}`} />
@@ -85,8 +109,7 @@ export const Jeu = () => {
       </div>
       {/* Boutons d'action */}
       <button onClick={() => {
-        deal(playerCards)
-        deal(dealerCards)}}>Deal</button>
+        deal();}}>Deal</button>
       <button onClick={() => hit(playerCards)}>Hit</button>
       <button onClick={stand}>Stand</button>
     </div>
