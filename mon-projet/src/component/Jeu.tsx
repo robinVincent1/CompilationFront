85d64e80b88703bf83a064b88511d06ea5b670fb
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2024-03-02 18:33:42 +0100

    Squashed commit of the following:
    commit 8cf6c77888558482f1f3b89c8ffc4fb9cfc1e8c0
    Author: Robin Vincent <robin.vincent@etu.umontpellier.fr>
    Date:   Sat Mar 2 18:32:48 2024 +0100
    
        css
    
    commit 145ec396fd8b4e87c4522fe9993eae9bbc4ef872
    Author: Robin Vincent <robin.vincent@etu.umontpellier.fr>
    Date:   Sat Mar 2 18:02:30 2024 +0100
    
        tailwind
    
    commit 68b1d8febab4737c7520c712f23a6c9bd5017e64
    Author: Robin Vincent <robin.vincent@etu.umontpellier.fr>
    Date:   Sat Mar 2 17:59:55 2024 +0100
    
        test tailwind
    
    commit ec6f8efd7bb3ea0533490a1ba038b1208cad05c3
    Author: Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
    Date:   Sat Mar 2 17:44:05 2024 +0100
    
        Update Jeu.tsx
    
    commit 17c6a7f108ec65fda6758f7e5f64974e7618bb31
    Author: Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
    Date:   Sat Mar 2 17:42:23 2024 +0100
    
        Update Jeu.tsx
    
    commit fe368c78b8264b751bb3fa2b06e4f6e5d917dd69
    Author: Robin Vincent <robin.vincent@etu.umontpellier.fr>
    Date:   Sat Mar 2 17:20:56 2024 +0100
    
        vasy gros

--- mon-projet/src/component/Jeu.tsx
+++ mon-projet/src/component/Jeu.tsx
@@ -1,3 +1,213 @@
+import { randomInt } from "crypto";
+import React, { useState } from "react";
+
+export const Jeu = () => {
+  const [Cards, setCards] = useState<string[]>([
+    "2-C.png",
+    "2-D.png",
+    "2-H.png",
+    "2-S.png",
+    "3-C.png",
+    "3-D.png",
+    "3-H.png",
+    "3-S.png",
+    "4-C.png",
+    "4-D.png",
+    "4-H.png",
+    "4-S.png",
+    "5-C.png",
+    "5-D.png",
+    "5-H.png",
+    "5-S.png",
+    "6-C.png",
+    "6-D.png",
+    "6-H.png",
+    "6-S.png",
+    "7-C.png",
+    "7-D.png",
+    "7-H.png",
+    "7-S.png",
+    "8-C.png",
+    "8-D.png",
+    "8-H.png",
+    "8-S.png",
+    "9-C.png",
+    "9-D.png",
+    "9-H.png",
+    "9-S.png",
+    "10-C.png",
+    "10-D.png",
+    "10-H.png",
+    "10-S.png",
+    "A-C.png",
+    "A-D.png",
+    "A-H.png",
+    "A-S.png",
+    "J-C.png",
+    "J-D.png",
+    "J-H.png",
+    "J-S.png",
+    "Q-C.png",
+    "Q-D.png",
+    "Q-H.png",
+    "Q-S.png",
+    "K-C.png",
+    "K-D.png",
+    "K-H.png",
+    "K-S.png",
+  ]);
+  const [dealerCards, setDealerCards] = useState<string[]>([]);
+  const [playerCards, setPlayerCards] = useState<string[]>([]);
+  const [dealerScore, setDealerScore] = useState<number>(0);
+  const [playerScore, setPlayerScore] = useState<number>(0);
+
+  const hit = (j: string[]) => {
+    let randomIndex = Math.floor(Math.random() * Cards.length);
+    let cardToAdd: string = Cards[randomIndex];
+    let updatedCards = [...Cards];
+
+    updatedCards.splice(randomIndex, 1);
+
+    if (j === dealerCards) {
+      setDealerCards([...dealerCards, cardToAdd]);
+      updateDealerScore([cardToAdd]);
+    } else if (j === playerCards) {
+      setPlayerCards([...playerCards, cardToAdd]);
+      updatePlayerScore([cardToAdd]);
+    }
+
+    setCards(updatedCards);
+  };
+
+  const updatePlayerScore = (cards: string[]) => {
+    let tempPlayerScore: number = playerScore;
+    for (let i = 0; i < cards.length; i++) {
+      let cardValue = cards[i].slice(0, cards[i].indexOf("-"));
+      if (cardValue === "A") {
+        tempPlayerScore += 11;
+      } else if (cardValue === "J" || cardValue === "Q" || cardValue === "K") {
+        tempPlayerScore += 10;
+      } else {
+        tempPlayerScore += parseInt(cardValue);
+      }
+    }
+    setPlayerScore(tempPlayerScore);
+  };
+
+  const updateDealerScore = (cards: string[]) => {
+    let tempdealerScore: number = dealerScore;
+    for (let i = 0; i < cards.length; i++) {
+      let cardValue = cards[i].slice(0, cards[i].indexOf("-"));
+      if (cardValue === "A") {
+        tempdealerScore += 11;
+      } else if (cardValue === "J" || cardValue === "Q" || cardValue === "K") {
+        tempdealerScore += 10;
+      } else {
+        tempdealerScore += parseInt(cardValue);
+      }
+    }
+    setDealerScore(tempdealerScore);
+  };
+
+  const deal = () => {
+    dealerCards: [] = [];
+    playerCards: [] = [];
+    let updatedCards = [...Cards];
+    for (let i = 0; i < 2; i++) {
+      let randomIndex = Math.floor(Math.random() * Cards.length);
+      let cardToAdd: string = Cards[randomIndex];
+      updatedCards.splice(randomIndex, 1);
+      dealerCards.push(cardToAdd);
+    }
+    updateDealerScore(dealerCards);
+    setDealerCards(dealerCards);
+
+    for (let i = 0; i < 2; i++) {
+      let randomIndex = Math.floor(Math.random() * Cards.length);
+      let cardToAdd: string = Cards[randomIndex];
+
+      updatedCards.splice(randomIndex, 1);
+      playerCards.push(cardToAdd);
+    }
+    updatePlayerScore(playerCards);
+    setPlayerCards(playerCards);
+    setCards(updatedCards);
+  };
+
+  const [isStand, setIsStand] = useState(false);
+
+  const stand = () => {
+    // Logique pour terminer le tour du joueur et laisser le croupier jouer
+    setIsStand(true)
+  };
+
+  return (
+    <div>
+      <h1 className="font-bold text-4xl p-8">Blackjack Game</h1>
+      <div className="border rounded">
+        <h2>Dealer's Hand {isStand && dealerScore}</h2>
+        <div className="flex justify-center">
+          {dealerCards.map((card, index) => (
+            <div className="p-8" key={index}>
+              {isStand ? (
+                <img
+                  className="w-32 h-auto"
+                  src={"/cards/" + card}
+                  alt={`Card ${index}`}
+                />
+              ) : index === 0 ? (
+                <img
+                  className="w-32 h-auto"
+                  src={"/cards/BACK.png"}
+                  alt={`Card ${index}`}
+                />
+              ) : (
+                <img
+                  className="w-32 h-auto"
+                  src={"/cards/" + card}
+                  alt={`Card ${index}`}
+                />
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Afficher les cartes du joueur */}
+      <div className="border rounded pb-">
+        <h2 className="">Player's Hand {playerScore}</h2>
+        <div className="flex justify-center">
+          {playerCards.map((card, index) => (
+            <div className="p-8" key={index}>
+              <img
+                className="w-32 h-auto"
+                src={"/cards/" + card}
+                alt={`Card ${index}`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Afficher les cartes du croupier */}
+      {/* Boutons d'action */}
+      <button
+        className="p-4 font-bold"
+        onClick={() => {
+          deal();
+        }}
+      >
+        Deal
+      </button>
+      <button className="p-4 font-bold" onClick={() => hit(playerCards)}>
+        Hit
+      </button>
+      {!isStand && (
+        <button className="font-bold" onClick={stand}>
+          Stand
+        </button>
+      )}
+    </div>
+  );
+};
 import { randomInt } from 'crypto';
 import React, { useState } from 'react';
 
