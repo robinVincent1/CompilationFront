01dc3e9788db39ac89b1438d34addc7abf04015c
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2024-03-07 17:43:03 +0100

    bet

--- mon-projet/src/component/Jeu.tsx
+++ mon-projet/src/component/Jeu.tsx
@@ -1,65 +1,97 @@
-import { randomInt } from 'crypto';
-import React, { useState,useEffect } from 'react';
-import { webSocketService } from './WebSocketService';
+import { randomInt } from "crypto";
+import React, { useState, useEffect } from "react";
+import { webSocketService } from "./WebSocketService";
 
 type Dealer = {
-  score : number ;
-  isWinner : boolean;
+  score: number;
+  isWinner: boolean;
   hand: string[]; //Cartes que le dealer posséde
-}
+};
 
 type Player = {
-  id : number;
-  pseudo : string;
-  wallet : number;
-  bet : number ; //Mise du joueur
-  isPlaying : number ; //Si le joueur est en train de jouer
-  score : number ;
-  isWinner : boolean;
-  isStanding : boolean;
+  id: number;
+  pseudo: string;
+  wallet: number;
+  bet: number; //Mise du joueur
+  isPlaying: number; //Si le joueur est en train de jouer
+  score: number;
+  isWinner: boolean;
+  isStanding: boolean;
   hand: string[]; //Cartes que le joueur posséde
-}
-
-
+};
 
 export const Jeu = () => {
-  const [dealer, setDealer] = useState<Dealer>({score:0, isWinner:false, hand:[]});
-  const [player, setPlayer] = useState<Player>({id:0, pseudo:"", wallet:0, bet:0, isPlaying:0, score:0, isWinner:false,isStanding:false, hand:[]});
- 
+  const [dealer, setDealer] = useState<Dealer>({
+    score: 0,
+    isWinner: false,
+    hand: [],
+  });
+  const [player, setPlayer] = useState<Player>({
+    id: 0,
+    pseudo: "",
+    wallet: 0,
+    bet: 0,
+    isPlaying: 0,
+    score: 0,
+    isWinner: false,
+    isStanding: false,
+    hand: [],
+  });
+
   useEffect(() => {
     const handleGameData = (data: any) => {
-      console.log("player" , data.eventData.player);
+      console.log("player", data.eventData.player);
       setDealer(data.eventData.dealer);
       setPlayer(data.eventData.player);
-    }
+    };
     webSocketService.connect("ws://localhost:8080/game", handleGameData);
+  }, []);
+
+  function hit() {
+    webSocketService.sendMessage(`${player.id}:hit`);
   }
-  , []);
 
+  function stand() {
+    webSocketService.sendMessage(`${player.id}:stand`);
+  }
 
-  
-function hit(){
-  webSocketService.sendMessage(`${player.id}:hit`);
-}
+  function deal() {
+    console.log("deal");
+    webSocketService.sendMessage(`${player.id}:deal`);
+  }
 
-function stand(){
-  webSocketService.sendMessage(`${player.id}:stand`);
-}
+  function reload() {
+    webSocketService.sendMessage(`${player.id}:reload`);
+  }
 
-function deal(){
-  console.log("deal")
-  webSocketService.sendMessage(`${player.id}:deal`);
-}
+  function bet() {
+    if (amount > 0 && amount <= player.wallet) {
+      // Vérification de la valeur de la mise
+      webSocketService.sendMessage(`${player.id}:bet:${amount}`);
+      setBetOk(true);
+      setBetIsShow(false);
+    } else {
+      setBetOk(false);
+    }
+  }
 
-function reload(){
-  webSocketService.sendMessage(`${player.id}:reload`);
-}
+  const [betIsShow, setBetIsShow] = useState(false);
+  const [amount, setAmount] = useState(0);
+  const handleBet = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(event.target.value);
+    if (!isNaN(value)) {
+      setAmount(value);
+    }
+  };
 
+  const [betOk, setBetOk] = useState(true);
 
   return (
     <div>
       <h1 className="font-bold text-4xl p-8">Blackjack Game</h1>
-      <p className='font-bold text-[red] text-2xl p-8'>{"A changer (winner) " }</p>
+      <p className="font-bold text-[red] text-2xl p-8">
+        {"A changer (winner) "}
+      </p>
       <div>
         <h2>Dealer's Hand {player.isStanding && dealer.score}</h2>
         <div className="flex justify-center">
@@ -89,14 +121,19 @@ function reload(){
         </div>
       </div>
       {/* Afficher les cartes du joueur */}
-      <div >
-        <h2 className=''> Player's Hand {player.score}</h2>
-        <div className='flex justify-center'>
-        {player.hand.map((card, index) => (
-          <div className='p-8' key={index}>
-            <img  className="w-32 h-auto" src={"/cards/" + card} alt={`Card ${index}`} />
-          </div>
-        ))}
+      <div>
+        <h2 className=""> Player's Hand {player.score}</h2>
+        <p>Mise : {player.bet}</p>
+        <div className="flex justify-center">
+          {player.hand.map((card, index) => (
+            <div className="p-8" key={index}>
+              <img
+                className="w-32 h-auto"
+                src={"/cards/" + card}
+                alt={`Card ${index}`}
+              />
+            </div>
+          ))}
         </div>
       </div>
       {/* Afficher les cartes du croupier */}
@@ -109,6 +146,36 @@ function reload(){
       >
         Deal
       </button>
+      {betIsShow ? (
+        <div>
+          <input
+            type="number"
+            className="text-black"
+            placeholder={` Max ${player.wallet}`}
+            onChange={handleBet}
+          />
+          <button
+            className="p-4 font-bold"
+            onClick={() => {
+              bet();
+            }}
+          >
+            Miser
+          </button>
+          {!betOk && amount > 0 && (
+            <p className="text-[red]">Vous n'avez pas assez d'argent</p>
+          )}
+        </div>
+      ) : (
+        <button
+          className="p-4 font-bold"
+          onClick={() => {
+            setBetIsShow(true);
+          }}
+        >
+          Bet
+        </button>
+      )}
       <button className="p-4 font-bold" onClick={() => hit()}>
         Hit
       </button>
